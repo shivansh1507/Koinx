4d425883b37e9e092aa13198fac46bb4e552e1c2
shivansh1507 <sharmashivansh462@gmail.com>
2024-03-07 20:57:03 +0530

    commit
    final commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import BitcoinPage from "./Bitcoin";
+const Home = () => {
+  return (
+          <BitcoinPage />
+  );
+};
+export default Home;
